be38d143bead70ab340c71bfaa896624c5ac2aec
SaajanM <saajan.maslanka@gmail.com>
2022-11-05 16:51:07 -0700

    API Based Projects

---
+++ personal-website/pages/api/get_projects.ts
@@ -0,0 +1,76 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import path from 'path';
+import { promises as fs } from 'fs';
+
+const validKeys = ["github", "deploy"] as const;
+type vKeyType = typeof validKeys[number];
+
+type Project = {
+  title: string,
+  description: string,
+  github?: string,
+  deployment?: string
+}
+
+type Category = {
+  heading: string,
+  description: string,
+  collapsible: boolean
+  projects: Project[]
+}
+
+export type Data = {
+  categories: Category[]
+}
+
+export default async function handler(
+  _: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const mdDirectory = path.join(process.cwd(), 'content');
+  const fileContents = await fs.readFile(mdDirectory + '/projects.md', 'utf8');
+
+  const categories = fileContents.split('---');
+  let response: Data = { categories: [] };
+  for (let c in categories) {
+    let category = categories[c].trim();
+    let [info, ...projects] = category.split('\n\n');
+    let infomatches = info.match(/^(?:# *)(\S+(?: \S*)*)\n([^\n]*)$/);
+    let heading = infomatches?.[1] || "NO HEADING";
+    let description = infomatches?.[2] || "NO DESCRIPTION";
+    let projectArray: Project[] = [];
+    for (let p in projects) {
+      let [rawheader, ...projectdesc] = projects[p].trim().split('\n');
+      let [rawtitle, meta] = rawheader.split('|');
+      let title = rawtitle.trim().match(/^(?:## *)(\S+(?: \S*)*)$/)?.[1] || "INVALID TITLE";
+
+      let metaTags: { [key in vKeyType]?: string } = {};
+      if (meta) {
+        meta = meta.trim();
+        let tags = meta.split(',');
+        for (let t in tags) {
+          let [key, val] = tags[t].split("=");
+          if (!(key && val)) continue;
+          key = key.trim()
+          if (!validKeys.includes(key as vKeyType)) continue;
+          let verifiedKey = key as vKeyType;
+          val = val.trim().match(/"(.*)"/)?.[1] || "NO VALUE";
+          metaTags[verifiedKey] = val;
+        }
+      }
+
+      let description = projectdesc.join(' ');
+      projectArray.push({
+        description,
+        title,
+        ...metaTags
+      });
+    }
+    response.categories.push({
+      heading, description, projects: projectArray, collapsible: true
+    });
+  }
+
+  res.send(response);
+}
--- personal-website/pages/projects.tsx
+++ personal-website/pages/projects.tsx
@@ -1,10 +1,36 @@
 import Project from "../components/Project/project";
 import ProjectCollection from "../components/ProjectCollection/projectcollection";
+import { Data } from "./api/get_projects"
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then(r => r.json())
+
+const mdToJSX = (data?: Data,) => {
+  if (!data) return;
+  let res: JSX.Element[] = []
+  for (let c = 0; c < data.categories.length; c++) {
+    let collection = data.categories[c];
+    let projects: JSX.Element[] = []
+    for (let p = 0; p < collection.projects.length; p++) {
+      let project = collection.projects[p];
+      projects.push(
+        <Project key={p} title={project.title} deploy={project.deployment} github={project.github}>{project.description}</Project>
+      )
+    }
+    res.push(
+      <ProjectCollection collapsible={collection.collapsible} key={c} heading={collection.heading} description={collection.description}>{projects}</ProjectCollection>
+    )
+  }
+  return res;
+}
 
 export default function Projects() {
+  const { data } = useSWR<Data>('/api/get_projects', fetcher)
+  const projects = mdToJSX(data);
+
   return (
     <>
-      <ProjectCollection collapsible heading="Current Projects" description="These projects are under works and do not have showcases">
+      {/* <ProjectCollection collapsible heading="Current Projects" description="These projects are under works and do not have showcases">
         <Project title="TCR-Epitope Binding Prediction with ML">
           I am developing a model to predict the binding affinity between TCR protein complexes
           and epitopes in the immune system with the ASU Biodesign Institute. This model
@@ -54,7 +80,8 @@ export default function Projects() {
           generated on the map showing directions and distance. This used Djikstra's
           algorithm to calculate pathing and uses React for the UI.
         </Project>
-      </ProjectCollection>
+      </ProjectCollection> */}
+      {projects}
     </>
   );
 }
